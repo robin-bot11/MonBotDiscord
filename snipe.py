70b311a52fd5978f934bdcdfe8c9775c5536a6b5
robin-bot11 <robinbot404@gmail.com>
2025-12-30 18:08:54 -0300

    snipe.py
    snipe.py

---
+++ snipe.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+class Snipe(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_deleted = None
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if not message.author.bot:
+            self.last_deleted = message
+
+    @commands.command()
+    async def snipe(self, ctx):
+        if self.last_deleted:
+            await ctx.send(f"Dernier message supprimé: {self.last_deleted.content}")
+        else:
+            await ctx.send("Rien à snipe pour le moment !")
+
+def setup(bot):
+    bot.add_cog(Snipe(bot))
