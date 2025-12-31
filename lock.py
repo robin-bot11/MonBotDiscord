7c112f7cd704b944f7e81750e0fe7f376ca5a2f5
robin-bot11 <robinbot404@gmail.com>
2025-12-31 19:02:01 -0300

    lock.py
    Update lock.py

--- lock.py
+++ lock.py
@@ -1,18 +1,57 @@
 from discord.ext import commands
+import discord
+from datetime import datetime
 
-class Lock(commands.Cog):
+warns = {}
+
+class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def lock(self, ctx, channel: commands.TextChannel):
-        await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        await ctx.send(f"{channel.mention} est maintenant verrouillé.")
+    async def check_member(self, ctx):
+        if not ctx.author.guild_permissions.administrator and ctx.author.id != 1383790178522370058:
+            await ctx.send(embed=discord.Embed(
+                description="Vous n'avez pas la permission nécessaire pour utiliser cette commande.",
+                color=0x6b00cb
+            ))
+            return False
+        return True
 
     @commands.command()
-    async def unlock(self, ctx, channel: commands.TextChannel):
-        await channel.set_permissions(ctx.guild.default_role, send_messages=True)
-        await ctx.send(f"{channel.mention} est maintenant déverrouillé.")
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member_id: int, *, reason=None):
+        member = ctx.guild.get_member(member_id)
+        if not member:
+            await ctx.send("Membre introuvable.")
+            return
+        await member.kick(reason=reason)
+        await ctx.send(embed=discord.Embed(
+            description=f"{member.name} a été expulsé. Raison: {reason}",
+            color=0x6b00cb
+        ))
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member_id: int, *, reason=None):
+        member = ctx.guild.get_member(member_id)
+        if not member:
+            await ctx.send("Membre introuvable.")
+            return
+        await member.ban(reason=reason)
+        await ctx.send(embed=discord.Embed(
+            description=f"{member.name} a été banni. Raison: {reason}",
+            color=0x6b00cb
+        ))
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def uban(self, ctx, member_id: int):
+        member = await ctx.guild.fetch_member(member_id)
+        await ctx.guild.unban(member)
+        await ctx.send(embed=discord.Embed(
+            description=f"{member} a été débanni.",
+            color=0x6b00cb
+        ))
 
 def setup(bot):
-    bot.add_cog(Lock(bot))
+    bot.add_cog(Moderation(bot))
