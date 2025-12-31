467c35356a092902b8cf1ae85145006c2d81dd3c
robin-bot11 <robinbot404@gmail.com>
2025-12-31 20:59:21 -0300

    lock.py
    Update lock.py

--- lock.py
+++ lock.py
@@ -1,35 +1,21 @@
 from discord.ext import commands
 import discord
 
-COLOR = 0x6b00cb
-
 class Lock(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     @commands.has_permissions(manage_channels=True)
-    async def lock(self, ctx, channel: discord.TextChannel = None):
-        channel = channel or ctx.channel
+    async def lock(self, ctx, channel: discord.TextChannel):
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-
-        embed = discord.Embed(
-            description=f"{channel.mention} est verrouillé.",
-            color=COLOR
-        )
-        await ctx.send(embed=embed)
+        await ctx.send(f"{channel.mention} est maintenant verrouillé.")
 
     @commands.command()
     @commands.has_permissions(manage_channels=True)
-    async def unlock(self, ctx, channel: discord.TextChannel = None):
-        channel = channel or ctx.channel
+    async def unlock(self, ctx, channel: discord.TextChannel):
         await channel.set_permissions(ctx.guild.default_role, send_messages=True)
-
-        embed = discord.Embed(
-            description=f"{channel.mention} est déverrouillé.",
-            color=COLOR
-        )
-        await ctx.send(embed=embed)
+        await ctx.send(f"{channel.mention} est maintenant déverrouillé.")
 
 def setup(bot):
     bot.add_cog(Lock(bot))
