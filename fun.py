a8898fab5ffbf9bc429886d916f9f4cbc316cf4e
robin-bot11 <robinbot404@gmail.com>
2026-01-01 10:20:11 -0300

    fun.py
    Update fun.py

--- fun.py
+++ fun.py
@@ -1,7 +1,4 @@
-# fun.py
 from discord.ext import commands
-
-COLOR = 0x6b00cb
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -9,14 +6,12 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def papa(self, ctx):
-        """Compliment au propriétaire du bot"""
-        message = (
+        await ctx.send(
             "Mon papa ? 𝐃𝐄𝐔𝐒\n"
             "Le légendaire pilier de ce serveur, inégalable en sagesse et en puissance.\n"
             "Ta présence illumine chaque discussion, et ton charisme inspire tout le monde.\n"
             "Aucun obstacle ne peut t'arrêter, tu es un véritable modèle pour tous !"
         )
-        await ctx.send(message)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
