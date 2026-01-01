22dd155e54b94ee31897be8c5de48bf1ababb8dd
robin-bot11 <robinbot404@gmail.com>
2025-12-31 21:00:40 -0300

    logs.py
    Update logs.py

--- logs.py
+++ logs.py
@@ -1,21 +1,14 @@
 from discord.ext import commands
-import discord
-
-COLOR = 0x6b00cb
 
 class Logs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.log_channels = {}
 
-    @commands.command()
-    async def setlog(self, ctx, log_type, channel: discord.TextChannel):
-        self.log_channels[log_type] = channel.id
-        embed = discord.Embed(
-            description=f"Logs **{log_type}** configurés dans {channel.mention}",
-            color=COLOR
-        )
-        await ctx.send(embed=embed)
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        print(f"[MESSAGE] {message.author}: {message.content}")
 
 def setup(bot):
     bot.add_cog(Logs(bot))
