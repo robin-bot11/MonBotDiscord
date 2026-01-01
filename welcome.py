11dac188347de22128e26fe49abd4fd9b95bdbdb
robin-bot11 <robinbot404@gmail.com>
2026-01-01 10:52:24 -0300

    welcome.py
    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,39 +1,37 @@
 from discord.ext import commands
 import discord
-
-COLOR = 0x6b00cb
+from database import Database
 
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.channel = None
-        self.message = "Bienvenue {user} sur {server} !"
+        self.db = Database()
+        self.welcome_channel = {}  # {guild_id: channel_id}
 
     @commands.command()
-    async def setwelcome(self, ctx, *, message):
-        self.message = message
-        await ctx.send(embed=discord.Embed(
-            description="Message de bienvenue configuré.",
-            color=COLOR
-        ))
-
-    @commands.command()
+    @commands.has_permissions(administrator=True)
     async def setwelcomechannel(self, ctx, channel: discord.TextChannel):
-        self.channel = channel
-        await ctx.send(embed=discord.Embed(
-            description=f"Salon défini : {channel.mention}",
-            color=COLOR
-        ))
+        """Configurer le salon pour le message de bienvenue"""
+        guild_id = str(ctx.guild.id)
+        self.welcome_channel[guild_id] = channel.id
+        self.db.set_welcome_channel(guild_id, channel.id)
+        await ctx.send(f"✅ Salon de bienvenue défini : {channel.mention}")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if self.channel:
-            msg = self.message.format(
-                user=member.mention,
-                server=member.guild.name,
-                members=member.guild.member_count
+        guild_id = str(member.guild.id)
+        channel_id = self.welcome_channel.get(guild_id) or self.db.get_welcome_channel(guild_id)
+
+        if not channel_id:
+            return
+
+        channel = member.guild.get_channel(channel_id)
+        if channel:
+            msg = (
+                f"Souhaitez la bienvenue à {member.mention} sur **{member.guild.name} !**\n"
+                f"Nous sommes maintenant {member.guild.member_count} sur le serveur !!"
             )
-            await self.channel.send(msg)
+            await channel.send(msg)
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
