efa00d2e369ea2670daadc9216df39a8b6ff91be
robin-bot11 <robinbot404@gmail.com>
2026-01-01 11:21:04 -0300

    snipe.py
    Update snipe.py

--- snipe.py
+++ snipe.py
@@ -1,40 +1,38 @@
-# snipe.py
 from discord.ext import commands
 import discord
+from database import db
+from datetime import datetime
 
 COLOR = 0x6b00cb
 
 class Snipe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.last_deleted = {}  # {guild_id: {channel_id: message}}
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         if message.author.bot:
             return
-        guild_id = message.guild.id
-        channel_id = message.channel.id
-        if guild_id not in self.last_deleted:
-            self.last_deleted[guild_id] = {}
-        self.last_deleted[guild_id][channel_id] = message
+        # Sauvegarder le message supprimé dans la DB
+        db.set_snipe(message.channel.id, {
+            "author": str(message.author),
+            "content": message.content,
+            "time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        })
 
     @commands.command()
     async def snipe(self, ctx):
         """Affiche le dernier message supprimé dans le salon."""
-        guild_id = ctx.guild.id
-        channel_id = ctx.channel.id
-
-        if guild_id in self.last_deleted and channel_id in self.last_deleted[guild_id]:
-            msg = self.last_deleted[guild_id][channel_id]
-            embed = discord.Embed(
-                title="Dernier message supprimé",
-                description=f"**Auteur :** {msg.author}\n**Message :** {msg.content}",
-                color=COLOR
-            )
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("Rien à afficher pour le moment dans ce salon !")
+        data = db.get_snipe(ctx.channel.id)
+        if not data:
+            return await ctx.send("Rien à afficher pour le moment dans ce salon !")
+        
+        embed = discord.Embed(
+            title="Dernier message supprimé",
+            description=f"**Auteur :** {data['author']}\n**Message :** {data['content']}\n**Heure (UTC) :** {data['time']}",
+            color=COLOR
+        )
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Snipe(bot))
