f2f0fbdd3738ab88a8a25f96824480715856b74c
robin-bot11 <robinbot404@gmail.com>
2026-01-01 16:00:43 -0300

    creator.py
    Update creator.py

--- creator.py
+++ creator.py
@@ -8,27 +8,30 @@ class Creator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def is_owner(self, ctx):
+        return ctx.author.id == OWNER_ID
+
     @commands.command()
     async def ping(self, ctx):
-        if ctx.author.id != OWNER_ID:
-            return
+        if not self.is_owner(ctx):
+            return await ctx.send("Vous n'êtes pas autorisé à utiliser cette commande.")
         await ctx.send("Le bot est en ligne et répond correctement.")
 
     @commands.command()
     async def dm(self, ctx, user_id: int, *, message):
-        if ctx.author.id != OWNER_ID:
-            return
+        if not self.is_owner(ctx):
+            return await ctx.send("Vous n'êtes pas autorisé à utiliser cette commande.")
         user = await self.bot.fetch_user(user_id)
         try:
             await user.send(message)
             await ctx.send(f"Message envoyé à {user}.")
-        except:
+        except discord.Forbidden:
             await ctx.send("Impossible d'envoyer le message à cet utilisateur.")
 
     @commands.command()
     async def backupconfig(self, ctx):
-        if ctx.author.id != OWNER_ID:
-            return
+        if not self.is_owner(ctx):
+            return await ctx.send("Vous n'êtes pas autorisé à utiliser cette commande.")
         try:
             self.bot.db.backup()
             await ctx.send("La configuration a été sauvegardée avec succès.")
@@ -37,8 +40,8 @@ class Creator(commands.Cog):
 
     @commands.command()
     async def restoreconfig(self, ctx):
-        if ctx.author.id != OWNER_ID:
-            return
+        if not self.is_owner(ctx):
+            return await ctx.send("Vous n'êtes pas autorisé à utiliser cette commande.")
         try:
             self.bot.db.restore()
             await ctx.send("La configuration a été restaurée avec succès.")
