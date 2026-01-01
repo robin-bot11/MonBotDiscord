ccb62458c6c5b530640dfc6ca6e41a7e9b214775
robin-bot11 <robinbot404@gmail.com>
2026-01-01 08:44:49 -0300

    snipe.py
    Update snipe.py

--- snipe.py
+++ snipe.py
@@ -11,10 +11,9 @@ class Snipe(commands.Cog):
             self.last_deleted = message
 
     @commands.command()
-    @commands.has_permissions(manage_messages=True)
     async def snipe(self, ctx):
         if self.last_deleted:
-            await ctx.send(f"Dernier message supprimé de {self.last_deleted.author} : {self.last_deleted.content}")
+            await ctx.send(f"Dernier message supprimé: {self.last_deleted.content}")
         else:
             await ctx.send("Rien à snipe pour le moment !")
 
